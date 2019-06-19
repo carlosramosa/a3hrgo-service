788c754c126597fe9071bdc0a1ee5e22f10d17b0
carlosramosa <carlosramos.cramos@gmail.com>
2019-06-20 01:35:35 +0200

    chore(): improve code

--- app/controllers/login/index.js
+++ app/controllers/login/index.js
@@ -1,46 +1,43 @@
 'use strict';
 
 const { generateResponse } = require('../../utils');
+const puppeteer = require('puppeteer');
 
 module.exports = {
 
-    login : (req, res) => {
-        const { body: { email, password } } = req;
-        const puppeteer = require('puppeteer');
+    login : ({ body: { email, password } }, res) => {
 
         puppeteer.launch().then(async browser => {
-        const page = await browser.newPage();
+            const page = await browser.newPage();
 
-        await page.goto('https://ecolex.a3hrgo.com');
+            await page.goto('https://ecolex.a3hrgo.com');
 
-        await page.type('input#Login', email);
-        await page.type('input#Password', password);
+            await page.type('input#Login', email);
+            await page.type('input#Password', password);
 
-        //let ok;
+            const [finalResponse, nothing ] = await Promise.all([
+                page.waitForResponse((response) => {
+                    return response.url() === 'https://ecolex.a3hrgo.com/Account/Login?ReturnUrl=%2F';
+                }),
+                page.click('input.btnLogin')
+            ]);
 
-        const [finalResponse, nothing ] = await Promise.all([
-            page.waitForResponse((response) => {
-                return response.url() === 'https://ecolex.a3hrgo.com/Account/Login?ReturnUrl=%2F';
-            }),
-            await page.click('input.btnLogin')
-        ]);
+            if (finalResponse.status() === 302) {
+                console.log('Login OK');
+                generateResponse(res)({ ok: true, message: 'Login OK' });
+            } else {
+                console.log('Login ERROR');
+                generateResponse(res)({ ok: false, message: 'Login KO' });
+            }
 
-        if (finalResponse.status() === 302) {
-            console.log('Login OK');
-            generateResponse(res)({ ok: true, message: 'Login OK' });
-        } else {
-            console.log('Login ERROR');
-            generateResponse(res)({ ok: false, message: 'Login KO' });
-        }
-
-        await browser.close()
-        
-        // const options = {
-        // path: 'a-header.png',
-        // fullPage: true
-        // }
-        // await page.screenshot(options);
-        // await browser.close();
+            await browser.close()
+            
+            // const options = {
+            // path: 'a-header.png',
+            // fullPage: true
+            // }
+            // await page.screenshot(options);
+            // await browser.close();
         });
 
     }
