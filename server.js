cdf0878f26d81cd8d1d9b9b29461eae20fc40de4
carlosramosa <carlosramos.cramos@gmail.com>
2019-06-20 01:49:49 +0200

    chore(server): add readme to front

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ figlet('Server Started', (err, data) =>
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+router.get('/', function(req, res, next) {
+    res.render('./README.md', { title: 'Express' });
+});
+
+
 const routesContext = {
     app
     , controller: require('./app/controllers')
