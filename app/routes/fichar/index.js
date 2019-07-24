42afdfea54dfde264f1dd1548297dd4620cbe301
carlosramosa <carlosramos.cramos@gmail.com>
2019-07-24 23:14:02 +0200

    chore: set main file fuck

--- app/routes/fichar/index.js
+++ app/routes/fichar/index.js
@@ -1,10 +1,8 @@
 'use strict';
 
-module.exports = ({ app, middleware, controller: { login } }) => {
+module.exports = ({ app, controller: { login } }) => {
 
     app.post('/fichar', login);
-    app.get('/', function(req, res, next) {
-        res.sendFile('./index.html', { title: 'Express' });
-    });
+
     module.exports = app; 
 }
